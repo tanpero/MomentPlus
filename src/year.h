a61662df37f54e3fe5c46d0499423033b86a9348
Tanpero <3185750139@qq.com>
2019-01-24 13:40:01 +0800

    define the class 'Year' in file 'src/year.h'

---
+++ src/year.h
@@ -0,0 +1,121 @@
+#ifndef _SRC_YEAR_H_
+#define _SRC_YEAR_H_
+
+#include "datestuff.h"
+
+namespace DateStuff
+{
+    class Year
+    {
+    public:
+	Year();
+	Year(int);
+	Year(const string&);
+
+	// Getter
+	int getYear() const;
+
+	// Comparing
+	int compare(const Year& right) const;
+	bool operator==(const Year& right) const;
+	bool operator!=(const Year& right) const;
+	bool operator<(const Year& right) const;
+	bool operator>(const Year& right) const;
+	bool operator<=(const Year& right) const;
+	bool operator>=(const Year& right) const;
+
+	// Computing
+	int operator-(const Year& right) const;
+
+	// Other operation
+	virtual void addYears(int years);
+	virtual void substractYears(int Years);
+
+	int yearsBetween(const Years& right) const;
+	Duration ageBetween(const Years& right) const;
+	Duration age() const;
+	int endOfYear() const;
+
+	std::string toString() const;
+	bool isValid() const;
+	bool isEmpty() const;
+
+    protected:
+	int m_year;
+    };
+
+    inline int Year::getYear() const
+    {
+	return m_year;
+    }
+
+    inline int Year::compare(const Year& right) const
+    {
+	return m_year - right.m_year;
+    }
+
+    inline bool Year::operator==(const Year& right) const
+    {
+	return compare(right) == 0;
+    }
+
+    inline bool Year::operator!=(const Year& right) const
+    {
+	return compare(right) != 0;
+    }
+
+    inline bool Year::operator<(const Year& right) const
+    {
+	return compare(right) < 0;
+    }
+
+    inline bool Year::operator>(const Year& right) const
+    {
+	return compare(right) > 0;
+    }
+
+    inline bool Year::operator<=(const Year& right) const
+    {
+	return compare(right) <= 0;
+    }
+
+    inline bool Year::operator>=(const Year& right) const
+    {
+	return compare(right) >= 0;
+    }
+
+    inline int Year::operator-(const Year& right) const
+    {
+	return compare(right);
+    }
+
+    inline Duration Year::ageBetween(const Year& right) const
+    {
+	return Duration(yearsBetween(right));
+    }
+
+    inline int Year::yearsBetween(const Year& right) const
+    {
+	int diff = static_cast<int>(right.m_year - m_year);
+	return static_cast<int>(diff >= 0 ? diff : -diff);
+    }
+
+    inline bool Year::isValid() const
+    {
+	return MIN_YEAR <= m_year && m_year <= MAX_YEAR;
+    }
+
+    inline bool Year::isEmpty() const
+    {
+	return m_year == 0;
+    }
+
+    inline std::string Year::toString() const
+    {
+	return isEmpty() ? std::string()
+			: DateStuff::toString(m_year, 0, 0);
+    }
+}	
+
+#endif // !_SRC_YEAR_H_
+
