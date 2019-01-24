4990e0d5d9933b263fdbd1be70291fb9480680ff
Tanpero <3185750139@qq.com>
2019-01-24 09:32:45 +0800

    Update the test example 'tdstuf.cpp'

--- test/tdstuf.cpp
+++ test/tdstuf.cpp
@@ -15,6 +15,15 @@ int main()
     cout.setf(ios::boolalpha);
     cout << "Date: " << month << '/' << day << '/' << year << endl;
     cout << "Leap year? " << isLeap(year) << endl;
+    cout << "Day of week: " << dayText[dayOfWeek(year, month, day)] << endl;
+    cout << "Day of year: " << dayOfYear(year, montu, day) << endl;
+    cout << "Days in month: " << endOfMonth(year, month) << endl;
+    cout << "Days in previous month: " << daysInPrevMonth(year, month) << endl;
+    cout << "Days in next month: " << daysInNextMonth(year, month) << endl;
+    cout << "3rd Saturday of month: " << nthWeekday(3, SATURDAY, year, month) << endl;
+    
+    long jday = greg2Jul(year, month, day);
+    cout << "Julian day number: " << jday << endl;
     
     return 0;
 }
